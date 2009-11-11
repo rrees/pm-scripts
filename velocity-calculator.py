536db0518628dc3cc86c302a58221a017bc4a902
rrees <spam@shudderfix.com>
2009-11-11 15:24:17 +0000

    Separated out a test for the base velocity calculation

--- velocity-calculator.py
+++ velocity-calculator.py
@@ -1,6 +1,6 @@
 from datetime import date, timedelta, date
 
-def calculate_weeks(points = 232, velocity = 5, team_capacity = 2):
+def calculate_weeks(points = 240 - 26, velocity = 5, team_capacity = 2):
 	return points / (velocity * team_capacity)
 
 def end_date(weeks, holiday_weeks = 2):
@@ -9,13 +9,13 @@ def end_date(weeks, holiday_weeks = 2):
 def format_date(date):
 	return date.strftime("%d/%m/%Y")
 
-instinct_velocity = 3
+yw_velocity = 6
 model_velocity = 5
 high_velocity = 10
 low_velocity = 7
 
 
-print "Yesterday's Weather Velocity (%d), end date: %s" % (instinct_velocity, format_date(end_date(calculate_weeks(velocity = instinct_velocity))))
+print "Yesterday's Weather Velocity (%d), end date: %s" % (yw_velocity, format_date(end_date(calculate_weeks(velocity = yw_velocity))))
 
 print "Model Velocity (%d), end date: %s" % (model_velocity,  format_date(end_date(calculate_weeks())))
 
---
+++ velocity-test.py
@@ -0,0 +1,3 @@
+from veolcity-calculator import calculate_weeks
+def test():
+	assert 2 == calculate_weeks(points = 20, velocity = 5, team_capacity = 2 )
