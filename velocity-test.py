912379f58c05b671296dbe9d0dfede209d59ee80
rrees <spam@shudderfix.com>
2009-11-18 15:54:29 +0000

    Created a velocity calculator maker

--- velocity-test.py
+++ velocity-test.py
@@ -1,4 +1,4 @@
-from velocity import calculate_weeks, end_date
+from velocity import velocity_calculator_maker, calculate_weeks, end_date
 from datetime import date
 
 def test_week_calcuation():
@@ -7,3 +7,8 @@ def test_week_calcuation():
 def test_end_date_calculation():
 	an_end_date = end_date(start_date = date(day = 18, month = 11, year = 2009), weeks = 1, holiday_weeks = 0)
 	assert 25 == an_end_date.day
+	
+def test_velocity_calculator_maker():
+	velocity_calculator = velocity_calculator_maker(target_points = 20, capacity = 2)
+	
+	assert 2 == velocity_calculator(velocity = 5)
--- velocity.py
+++ velocity.py
@@ -6,3 +6,5 @@ def calculate_weeks(points = 221 - 26, velocity = 5, team_capacity = 2):
 def end_date(weeks, holiday_weeks = 2, start_date = date.today()):
 	return start_date + timedelta(weeks = weeks + holiday_weeks)
 
+def velocity_calculator_maker(target_points, capacity):
+	return lambda total_points = target_points, velocity = 5, team_capacity = capacity: total_points / (velocity * team_capacity)
