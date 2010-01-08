bb164367ef1335334ec23662274bd9e132230848
Robert Rees <rrees@thoughtworkers-macbook-pro-7.local>
2010-01-08 17:09:31 +0000

    Added yesterday's weather to the project object

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 class Project:
-	def __init__(self, total_scope = 0, dev_complete = 0):
+	def __init__(self, total_scope = 0, dev_complete = 0, yesterdays_weather = 0):
 		self.total_scope = total_scope
 		self.dev_complete = dev_complete
+		
+		self.yesterdays_weather = yesterdays_weather
 		
 		self.remaining_scope = self.total_scope - self.dev_complete
--- tests/project-test.py
+++ tests/project-test.py
@@ -2,4 +2,9 @@
 from project import Project
 
 def test_project_should_calculate_the_remaining_scope():
-	pass
+	project = Project(total_scope = 60, dev_complete = 23)
+	assert project.remaining_scope == (60 - 23)
+
+def test_project_should_have_yesterdays_weather():
+	project = Project(yesterdays_weather = 33)
+	assert project.yesterdays_weather == 33
